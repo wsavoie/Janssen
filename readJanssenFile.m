2ac7e153ea6f4ceb2911ae317a90dcb18751fc7a
Will Savoie <wsavoie@gatech.edu>
2016-09-21 10:06:27 -0400

    removed some stuff to read from janssenCyl.in file results

--- readJanssenFile.m
+++ readJanssenFile.m
@@ -1,13 +1,17 @@
-function [data,fzS,lfzS] = readJanssenFile(dataFold)
+% function [data,fzS,lfzS,stressSides] = readJanssenFile(dataFold)
+function [data,fzS] = readJanssenFile(dataFold)
 if(dataFold(end)~='/')
     dataFold(end+1)='/';
 end
 
 force=importdata(horzcat(dataFold,'forces.txt'));
-fzS=force.data(1,4);
+fzS=abs(force.data(1,4));
 
-forceLang=importdata(horzcat(dataFold,'forcesLang.txt'));
-lfzS=forceLang.data(2:end,4);
+% forceLang=importdata(horzcat(dataFold,'forcesLang.txt'));
+% lfzS=forceLang.data(2:end,4);
+% 
+% sides=importdata(horzcat(dataFold,'cylForces.txt'));
+% stressSides=sides.data(2:4);
 
 data=readdump_all(horzcat(dataFold,'lastframePour.txt'));
 end
