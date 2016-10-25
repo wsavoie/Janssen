a3d394c702fb3cc7fe468d710757f1ac26464292
Will Savoie <wsavoie@gatech.edu>
2016-10-25 08:50:55 -0400

    adding two files, one with a janssen system with no walls the other reads a specific type of file output

---
+++ readCrushFile.m
@@ -0,0 +1,27 @@
+function [ varargout ] = readCrushFile( dataFold,type )
+%READCRUSHFILE reads and outputs data from lammps janssen data
+%   reads and outputs data from lammps janssen data where a mass is
+%   generated on top of GM which has already been settled
+
+if(dataFold(end)~='/')
+    dataFold(end+1)='/';
+end
+
+
+switch type
+    case 1
+        nOutputs = 1;%[data,fxS,fyS,fzS]
+        varargout = cell(1,nOutputs);
+        
+        force=importdata(horzcat(dataFold,'crushforces.txt'));
+        force = abs(force.data);
+        varargout{1}=force;
+        
+        data=readdump_all(horzcat(dataFold,'lastFrameCrush.txt'));
+        varargout{2}=data;
+end
+
+
+
+end
+
