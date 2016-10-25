23fa6f8afc870f24ea7f1e8a880de90f787441df
Will Savoie <wsavoie@gatech.edu>
2016-10-25 08:51:15 -0400

    small changes to run params in batch run file

--- runBatchLammps.sh
+++ runBatchLammps.sh
@@ -5,10 +5,11 @@ radArr=(4.0 5.0 6.0 7.0 16.0 6.0 6.0 6.0 6.0 6.0 6.0);
 # tStepsArr=(12.0 13.0 14.0 15.0 16.0); #slope for linear curve relating particle amount and pourSteps
 # fillP=(0.2 0.4 0.6 0.8);
 # fillP=(0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 .5 .6 .7 .8 0.2 0.3 0.4 0.5 0.6 0.7 0.85);
-fillP=(0.05 0.1 0.2 0.3 0.5 0.7 0.8 0.875 .9 .8 0.2 0.3 0.4 0.5 0.6 0.7 0.85);
+# fillP=(0.05 0.1 0.15 0.20 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 .9 .8 0.2 0.3 0.4 0.5 0.6 0.7 0.85);
+fillP=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8);
 # for rads in `seq 0 14`; do
-for rads in `seq 1 1`; do
-	for n in `seq 0 7`; do
+for rads in `seq 2 3`; do
+	for n in `seq 0 6`; do #0-7
 		h=75;
 		phi=0.59;
 		r=${radArr[$rads]}
