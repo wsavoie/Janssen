982c995255b0d9ba1c65d50aa3d589ceff916a09
WS <ws@phys42232.physics.gatech.edu>
2016-11-01 17:19:16 -0400

    allow staple shape in stiff batch and cleaned up

--- runBatchLammpsStiff.sh
+++ runBatchLammpsStiff.sh
@@ -5,7 +5,8 @@ radArr=(7.0 8.0 9.0 10.0 6.0 6.0 6.0 6.0 6.0);
 # tStepsArr=(12.0 13.0 14.0 15.0 16.0); #slope for linear curve relating particle amount and pourSteps
 # fillP=(0.2 0.4 0.6 0.8);
 # fillP=(0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 .5 .6 .7 .8 0.2 0.3 0.4 0.5 0.6 0.7 0.85);
- fillP=(442 885 1327 1770 2212 2655 3097 3540 3982 4425 4867 5310 5752 6195 6637 7080 7522);
+#  fillP=(442 885 1327 1770 2212 2655 3097 3540 3982 4425 4867 5310 5752 6195 6637 7080 7522);
+fillP=(221 443 664 885 1106 1328 1549 1770 1991 2213 2434 2655 2876 3098 3319 3540 3761);
 #fillP=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8);
 # for rads in `seq 0 14`; do
 fname="${1}";
@@ -13,14 +14,14 @@ cp ~/Janssen/Dimers/${fname} ~/Janssen/${fname}
 for rads in `seq 3 3`; do
 	for n in `seq 0 16`; do #0-7
 		h=100;
-		phi=0.59;
+		phi=0.26;
 		r=${radArr[$rads]}
 		np=${fillP[$n]};
 		# np=$(python -c "print(int(6*${phi}*${radArr[$rads]}*${radArr[$rads]}*${h}*${fillP[$n]}))");
 		echo $np
 		# ss=$(python -c "print(int(150*${np}))"); 
 		# ss=$(python -c "print(int(3162*${np}/(((${radArr[$rads]}-2.0)*3)*(${radArr[$rads]}-2.0)*3)+40000))"); 
-		ss=$(python -c "print(int(5000*${np}*4/(${r}*${r}*6-12*${r}+6)+50000))"); 
+		ss=$(python -c "print(int(10000*${np}*8/(${r}*${r}*6-12*${r}+6)+50000))"); 
 		echo $ss
 		# lmp_serial -var a ${aArr[$step]} -var cylInRad ${radArr[$rads]} < in.janssen
 		# lmp_serial -var tSteps ${ss} -var R ${radArr[$rads]} -var nParts ${np} < in.janssen
