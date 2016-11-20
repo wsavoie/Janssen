f90227728e1b2a888f8313b96d287f18b6464b99
WS <ws@phys42232.physics.gatech.edu>
2016-11-20 14:24:27 -0500

    edited to make shaking lammps files

--- MatlabCode/createDimer.m
+++ MatlabCode/createDimer.m
@@ -1,6 +1,6 @@
 % create dimer
-
-l=36;
+% for i=0:36
+l=38;
 w=28;
 
 N=2*l+w;
@@ -8,16 +8,18 @@ t=[90,90]; deg=1; %setting theta is degrees
 
 diam=0.5;
 type=3;
-mass=0.5236;
+mass=0.0654;
 overlap=0.5;
 %create matrices for generation
 pos=createStaple(diam,l,w,t,deg,overlap);
 types=type*ones(1,N);
 masses=mass*ones(1,N);
 diams=diam*ones(1,N);
-filename=['a1=',num2str(t(1)),'_a2=',num2str(t(2)),'_l=',num2str(l),'_w=',num2str(w),'_d=',num2str(diam),'_o=',num2str(overlap)];
+% filename=['a1=',num2str(t(1)),'_a2=',num2str(t(2)),'_l=',num2str(l),'_w=',num2str(w),'_d=',num2str(diam),'_o=',num2str(overlap)];
+filename=['w=',num2str(w),'_l=',num2str(l)];
 fold='/home/ws/Janssen/Dimers/';
 
 [momI,com]=CalcMomentOfInertiaAndCOM(pos,masses);
 
 generateLammpsDimer( pos,types,diams,masses,com,momI,filename,fold);
+% end
