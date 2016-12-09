a0ff345148e17170b8442d99d34ae35ddfe84e14
WS <ws@phys42232.physics.gatech.edu>
2016-12-09 11:01:49 -0500

    nondimensionalize delta sigma

--- MatlabCode/plotDeltaSig.m
+++ MatlabCode/plotDeltaSig.m
@@ -62,13 +62,15 @@ sysInfo=[0.5,0.5,0.25];
 [psig,psigS]=parsed2Sigma(out,outForce,sysInfo);
 [csig,csigS]=parsed2Sigma(out2,outForce2,sysInfo);
 
-dsigA=(csig-psig); dsigAS=(csigS-psigS);
+dsigA=(csig-psig)./(psig); dsigAS=(csigS-psigS)./(psigS);
 [dsigrr dsigzz]=separateVec(dsigA,1);
 [dsigrrS dsigzzS]=separateVec(dsigAS,1);
 A1=[-100:20:100]; A2=[-100:20:100];  [A1q,A2q]=meshgrid(A1,A2);
 
 dsigrrq=griddata(a1,a2,dsigrr,A1q,A2q);
 dsigrrSq=griddata(a1,a2,dsigrrS,A1q,A2q);
+dsigzzq=griddata(a1,a2,dsigzz,A1q,A2q);
+dsigzzSq=griddata(a1,a2,dsigzzS,A1q,A2q);
 
 figure(23);
 hold on;
@@ -76,11 +78,12 @@ title('\Delta\sigma_{rr}');
 plot3(a1,a2,dsigrr,'o');
 mesh(A1q,A2q,dsigrrq);
 xlabel('\alpha_1');ylabel('\alpha_2');zlabel('\Delta\sigma_{rr}');
-
+% axis([-130 130 -130 130 0,1000]);
 
 figure(24);
 hold on;
-title('\Delta\sigma_{rr}_{sim} ');
-plot3(a1,a2,dsigrrS,'o');
-mesh(A1q,A2q,dsigrrSq);
+title('\Delta\sigma_{zz} ');
+plot3(a1,a2,dsigzz,'o');
+mesh(A1q,A2q,dsigzzq);
 xlabel('\alpha_1');ylabel('\alpha_2');zlabel('\Delta\sigma_{rr}');
+% axis([-130 130 -130 130 0,5]);
